4cff967ee0facccb94db879a144a3a286e49f372
raulpenate <50472064+raulpenate@users.noreply.github.com>
2024-12-25 20:56:49 -0600

    ✨ Classes

---
+++ 02-pointers-n-classes/03-classes.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Cookie
+{
+private:
+    string color;
+
+public:
+    Cookie(string color)
+    {
+        this->color = color;
+    }
+
+    string getColor()
+    {
+        return color;
+    }
+    void setColor(string color)
+    {
+        this->color = color;
+    }
+};
+
+int main()
+{
+    //  instanciating a new cookie object with color green in  pointer of an object type Cookie*
+    Cookie *cookieOne = new Cookie("green");
+    Cookie *cookieTwo = new Cookie("blue");
+
+    cookieOne->setColor("yellow");
+
+    cout << cookieOne->getColor() << endl;
+    cout << cookieTwo->getColor() << endl;
+
+    delete cookieOne; // Free the allocated memory
+    delete cookieTwo;
+}
